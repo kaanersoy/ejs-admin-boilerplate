d5bf8292612ea6edd932b8fd56cbb0d4f198e0e9
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-31 21:58:55 +0300

    Add comment-lines

--- routes/base.js
+++ routes/base.js
@@ -1,10 +1,12 @@
 const baseRouter = require('express').Router();
 const Database = require('../services/database');
 
+// Renders views/index.ejs with empty data!
 baseRouter.get('/', (req, res) => {
   res.render('index', {});
 });
 
+// Renders views/about.ejs with empty data!
 baseRouter.get('/about', (req, res) => {
   res.render('about', {});
 });
