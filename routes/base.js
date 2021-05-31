cbdd1b18e598630ba2af7bbd5366834a3e21b4be
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-31 21:34:34 +0300

    Add base website template routes

--- routes/base.js
+++ routes/base.js
@@ -1,12 +1,12 @@
 const baseRouter = require('express').Router();
+const Database = require('../services/database');
 
-baseRouter.get('/', (req,res) => {
-  res.render('index', {data: [
-    'yey',
-    'string',
-    'template'
-  ]})
-})
+baseRouter.get('/', (req, res) => {
+  res.render('index', {});
+});
 
+baseRouter.get('/about', (req, res) => {
+  res.render('about', {});
+});
 
-module.exports = baseRouter
+module.exports = baseRouter;
