135ee4cab1c28e297068bf3549906c9dc514f454
Kaan Ersoy <kaanersoy12@gmail.com>
2021-06-01 17:11:10 +0300

    Add create user route

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,12 @@ const Database = require('../services/database');
 const database = new Database();
 
 apiRouter.post('/register', async (req, res) => {
-  const data = {
-    username: 'kaan',
-    password: 'password'
+  const user = {
+    username: req.body.username,
+    password: req.body.password
   };
-  database.createUser(data);
-  res.send('yey');
+  const newUser = await database.createUser(user);
+  res.send(newUser).status(402);
 });
 
 apiRouter.get('/', async (req, res) => {
